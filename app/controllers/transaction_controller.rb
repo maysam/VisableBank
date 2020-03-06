88b2ceff0a2991af411622a3a9cc0cf11a90e07c
Maysam Torabi <kherad@gmail.com>
2020-03-06 04:07:49 +0330

    making bank transfer transactions thread-safe

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -2,38 +2,51 @@
 
 class TransactionController < ApplicationController
   def transfer
-    from_account = Account.find_by_account_number params[:from]
-    to_account = Account.find_by_account_number params[:to]
+    source = Account.find_by_account_number params[:from]
+    destination = Account.find_by_account_number params[:to]
     amount = params[:amount].to_i
     user_description = params[:user_description]
 
+    if source.nil?
+      return render status: :not_found, body: 'Sender account number is invalid'
+    end
+    if destination.nil?
+      return render status: :not_found, body: 'Receiver account number is invalid'
+    end
+
     if amount <= 0
       render status: :precondition_failed, body: 'Positive amount value is required'
-    elsif from_account.nil?
-      render status: :not_found, body: 'Sender account number is invalid'
-    elsif to_account.nil?
-      render status: :not_found, body: 'Receiver account number is invalid'
+    elsif source.balance_cents < amount
+      render status: :precondition_failed, body: "Available funds don't cover this transfer"
     else
       Transaction.transaction do
-        from_account.balance_cents -= amount
-        from_account.save!
+        Account.lock.find(source.id, destination.id)
+        # locking both accounts
 
-        from_account.transactions.create sent_cents: amount,
-                                         balance_cents: from_account.balance_cents,
-                                         user_description: user_description,
-                                         system_description: "sent to '#{to_account.account_number}'"
-        to_account.balance_cents += amount
-        to_account.save!
-        to_account.transactions.create received_cents: amount,
-                                       balance_cents: to_account.balance_cents,
-                                       user_description: user_description,
-                                       system_description: "received from '#{from_account.account_number}'"
+        source.balance_cents -= amount
+        source.save!
+
+        params = {
+          sent_cents: amount,
+          balance_cents: source.balance_cents,
+          user_description: user_description,
+          system_description: "sent to '#{destination.account_number}'"
+        }
+        source.transactions.create! params
+
+        destination.balance_cents += amount
+        destination.save!
+
+        params = {
+          received_cents: amount,
+          balance_cents: destination.balance_cents,
+          user_description: user_description,
+          system_description: "received from '#{source.account_number}'"
+        }
+        destination.transactions.create! params
+
         head :ok
       end
-    end
-  rescue ActiveRecord::RecordInvalid => err
-    if err.message == 'Validation failed: Balance cents must be greater than or equal to 0'
-      render status: :precondition_failed, body: "Available funds don't cover this transfer"
-    end
+  end
   end
 end
